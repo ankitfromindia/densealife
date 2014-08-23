d8e8243a1101b3b0376cb1c8f0827434732c307e
Ankit Vishwakarma <ankit.vishwakarma@jabong.com>
2014-08-24 00:39:35 +0530

    0000025: EVENT PAGE:Remove names under photos

--- addons/default/themes/densealife/views/modules/eventsmanager/albums.php
+++ addons/default/themes/densealife/views/modules/eventsmanager/albums.php
@@ -1,16 +1,16 @@
 <?php if ( $albums ): ?>
     <div class="comman-heading">Albums</div>
     <ul class="videos">
-        <?php foreach ( $albums as $album ): ?>
+        <?php foreach ( $albums as $album ):?>
             <?php if ( $album->count_files != 0 ): ?>
                 <li>
-                    <a class="fancybox fancybox.ajax" href="../../eventsmanager/album_images/<?php echo $album->id ; ?>">
+                    <a class="fancybox fancybox.ajax" href="/eventsmanager/album_images/<?php echo $album->id ; ?>">
                         <?php if ( !empty($album->cover) ): ?>
-                            <img src="<?php echo $album->cover ; ?>" />
+                            <img src="<?php echo $album->cover ; ?>" width="135" height="135" />
                         <?php else: ?>
                             {{ theme:image file="no-image.jpg"}} 
                         <?php endif ; ?>
-                        <span class="name"><?php echo strstr($album->name, ':', true) ; ?> (<?php echo $album->photo_count ; ?> Photos)</span>
+                        <span class="name"><?php echo $album->name; ?> (<?php echo $album->photo_count ; ?> Photos)</span>
                     </a>
                 </li>
             <?php endif ; ?>
@@ -24,10 +24,10 @@
         <?php foreach ( $photos as $image ): ?>
             <li>
                 <a class="fancybox fl mr10" data-fancybox-group="gallery" href="<?php echo $image->path ; ?>">
-                    <img src="{{url:site}}files/medium/<?php echo $image->id ; ?>" alt="" width="200" height="200"/>
+                    <img src="{{url:site}}files/thumb/<?php echo $image->id ; ?>/135/135/fit" alt="" width="135" height="135"/>
                 </a>
-                <span class='name'><?php echo strstr($image->name, '.', true) ; ?></span>
+<!--                <span class='name'><?php echo strstr($image->name, '.', true) ; ?></span>-->
             </li>
         <?php endforeach ; ?>
     </ul>
-<?php endif ; ?>
+<?php endif ;
